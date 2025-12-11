7f4930a1c9ca5e4ddceffc660030b3268be73df0
Angelica0918 <20243ds136@utez.edu.mx>
2025-12-11 14:41:05 -0600

    agregue el MapViewModel.kt

--- app/src/main/java/com/example/tripcam/utils/uriToFile.kt
+++ app/src/main/java/com/example/tripcam/utils/uriToFile.kt
@@ -1,2 +1,26 @@
 package com.example.tripcam.utils
 
+import android.content.Context
+import android.net.Uri
+import java.io.File
+import java.io.FileOutputStream
+
+fun uriToFile(context: Context, uri: Uri): File {
+    // Abre el flujo de entrada de la URI
+    val inputStream = context.contentResolver.openInputStream(uri)
+
+    // Crea un archivo temporal en el directorio cache (ej. upload_1234.jpg)
+    val file = File.createTempFile("upload_", ".jpg", context.cacheDir)
+
+    // Crea un flujo de salida para escribir en el archivo temporal [cite: 62]
+    val output = FileOutputStream(file)
+
+    // Copia el contenido del flujo de entrada al flujo de salida [cite: 62]
+    inputStream?.copyTo(output)
+
+    // Cierra los flujos
+    output.close()
+    inputStream?.close()
+
+    return file
+}
--- app/src/main/java/com/example/tripcam/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/example/tripcam/viewmodel/MapViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.tripcam.viewmodel
 
-class MapViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tripcam.data.db.LocationPoint
+import com.example.tripcam.data.repository.TripRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+
+class MapViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = TripRepository(application)
+    // Simplemente obtenemos TODOS los puntos y los convertimos en un StateFlow
+    val allTripPoints: StateFlow<List<LocationPoint>> =
+        repository.getAllPoints()
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(5000),
+                emptyList()
+            )
+
 }
