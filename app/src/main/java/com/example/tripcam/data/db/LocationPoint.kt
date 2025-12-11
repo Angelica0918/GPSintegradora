acd21fc7027a5e8fa0caddfb4ba820c29da15ae5
Monserrattorress <20243ds147@utez.edu.mx>
2025-12-11 13:58:58 -0600

    agregue el locationpoint

--- app/src/main/java/com/example/tripcam/data/db/LocationPoint.kt
+++ app/src/main/java/com/example/tripcam/data/db/LocationPoint.kt
@@ -1,4 +1,21 @@
 package com.example.tripcam.data.db
-
-class LocationPoint {
-}
+import androidx.room.Entity
+import androidx.room.ForeignKey
+@Entity(
+    tableName = "location_points",
+    primaryKeys = ["tripId", "timestamp"], // Clave única
+    foreignKeys = [
+        ForeignKey(
+            entity = Trip::class,
+            parentColumns = ["id"],
+            childColumns = ["tripId"],
+            onDelete = ForeignKey.CASCADE // Si borras un Trip, se borran sus puntos
+        )
+    ]
+)
+data class LocationPoint(
+    val tripId: Long,
+    val latitude: Double,
+    val longitude: Double,
+    val timestamp: Long
+)
